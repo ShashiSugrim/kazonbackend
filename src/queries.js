97a875f85a9eb17328830dc636aa84517bd0f0cf
Shashi Sugrim <ssugrim1430@bths.edu>
2023-08-10 21:27:07 -0400

    working search query

--- src/queries.js
+++ src/queries.js
@@ -2,12 +2,16 @@ const getProducts = "SELECT * FROM products;";
 const getProductById = "SELECT * FROM products where id=$1";
 const checkUserExists = "SELECT * FROM users WHERE email=$1";
 const addUser = "INSERT INTO USERS(email, password) VALUES ($1, $2)";
-const searchQuery = "SELECT * FROM PRODUCTS WHERE ";
+const searchQuery = "SELECT * from PRODUCTS where $1 = ANY(topics);";
+const getCartQuery = "SELECT * FROM CART WHERE email =$1";
+const createReportQuery = "";
 
 module.exports = {
   getProducts,
   getProductById,
   checkUserExists,
   addUser,
-  searchQuery
+  searchQuery,
+  getCartQuery,
+  createReportQuery
 };
