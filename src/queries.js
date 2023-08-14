0b816cf099cadb632c3c13c90464bff39ee0cb11
Kunsang Tsering <cozyboykt@Kunsangs-MacBook-Air.local>
2023-08-14 17:40:55 -0400

    Completed the queries

--- src/queries.js
+++ src/queries.js
@@ -4,7 +4,7 @@ const checkUserExists = "SELECT * FROM users WHERE email=$1";
 const addUser = "INSERT INTO USERS(email, password) VALUES ($1, $2)";
 const searchQuery = "SELECT * from PRODUCTS where $1 = ANY(topics);";
 const getCartQuery = "SELECT * FROM CART WHERE email =$1";
-const createReportQuery = "";
+const createReportQuery = "INSERT INTO reports(contentId, reason, reporterEmail) VALUES ($1, $2, $3)";
 
 module.exports = {
   getProducts,
