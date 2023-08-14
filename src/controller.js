f071ceef87eac0ee675f2435d94cc2f6cc9066de
Kunsang Tsering <cozyboykt@Kunsangs-MacBook-Air.local>
2023-08-14 17:40:15 -0400

    if statements to check if user gets over 5 report limits

--- src/controller.js
+++ src/controller.js
@@ -99,9 +99,20 @@ const getCart = async (req, res) =>
 
 }
 
-const createReport = (req, res) =>
+const createReport = async (req, res) =>
 {
   let email = req.user.email;
+  let userReportCountResult = await pool.query(queries.getUserReportCount, [email]).rows[0].count;
+  // Code below checks if user gets over 5 report limit
+  if (userReportCount >= 5){
+    return res.status(403).json ({
+      error: "User has reached the max report limit"
+    });
+  }
+  // Code below : have to insert the report data in the database
+  //Inserting the report in the database
+  await pool.query(queries.createReportQuery, [contentId, reason, email]);
+console.error("Report creating Error: ", error);
 
 }
 
